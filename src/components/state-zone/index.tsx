7f6c3efe65f8216c29d684c1466d8b09157b3e0d
YXS-2000 <1984570040@qq.com>
2022-04-06 21:32:35 +0800

    enhance the design of the banner

---
+++ src/components/state-zone/index.tsx
@@ -0,0 +1,13 @@
+import classNames from "classnames";
+import styles from "./state-zone.module.scss";
+
+type StatezoneProps = {
+    reverse: boolean,
+
+}
+export default function Statezone(props: StatezoneProps){
+
+    return (
+        <div></div>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,15 @@
 import Banner from "../components/banner"
+import { useNavigate } from "react-router-dom"
 
 export default function Index(){
+    const navigationTo = useNavigate();
 
     return (
-        <Banner></Banner>
+        <Banner
+            welcomeCn="欢迎"
+            welcomeEn="welcome"
+            stateContent="华南师范大学图书馆"
+            onClick={()=>navigationTo("/activities")}
+        ></Banner>
     )
 }
