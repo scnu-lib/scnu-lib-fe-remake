9f63bc17c7ff064271f1399d11dcba8cfd6c6004
YXS-2000 <1984570040@qq.com>
2022-04-07 21:57:56 +0800

    create some components

--- src/components/state-zone/index.tsx
+++ src/components/state-zone/index.tsx
@@ -3,11 +3,27 @@ import styles from "./state-zone.module.scss";
 
 type StatezoneProps = {
     reverse: boolean,
-
+    svgSrc: string,
+    titleCn: string,
+    titleEn: string,
+    content: string
 }
 export default function Statezone(props: StatezoneProps){
 
     return (
-        <div></div>
+        <div className={classNames(styles.stateContainer, {[styles.flexReverse]: props.reverse})}>
+            <div className={classNames(styles.introduceContainer)}>
+                <p className={styles.titleTextCn}>{props.titleCn}</p>
+                <p className={styles.titleTextEn}>{props.titleEn}</p>
+                <p className={styles.contentText}>{props.content}</p>
+            </div>
+            <div className={classNames(styles.pictureContainer)}>
+                <div 
+                    style={{backgroundImage: `url(${props.svgSrc})`}}
+                    className={classNames(styles.backgroundImg)}
+                ></div>
+                <div className={classNames(styles.backgroundImgShadow)}></div>
+            </div>
+        </div>
     )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,26 @@
-import Banner from "../components/banner"
-import { useNavigate } from "react-router-dom"
+import Banner from "../components/banner";
+import Statezone from "../components/state-zone";
+import { useNavigate } from "react-router-dom";
+import Study from "../source/undraw_bibliophile_re_xarc.svg";
 
 export default function Index(){
     const navigationTo = useNavigate();
 
     return (
-        <Banner
-            welcomeCn="欢迎"
-            welcomeEn="welcome"
-            stateContent="华南师范大学图书馆"
-            onClick={()=>navigationTo("/activities")}
-        ></Banner>
+        <div style={{width: '100%', display: 'flex', flexDirection: 'column'}}>
+            <Banner
+                welcomeCn="欢迎"
+                welcomeEn="welcome"
+                stateContent="华南师范大学图书馆"
+                onClick={()=>navigationTo("/activities")}
+            ></Banner>
+            <Statezone
+                reverse={false}
+                titleCn={"关于我们"}
+                titleEn={"About us"}
+                content={"我们是华南师范大学图书馆管理平台，在这里可以获取到关于图书馆的活动信息"}
+                svgSrc={Study}
+            ></Statezone>
+        </div>
     )
 }
