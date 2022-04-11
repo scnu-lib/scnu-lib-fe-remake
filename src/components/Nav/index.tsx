bc860ed2ee7da01baf2ac30594e8bb532cdf2865
YXS-2000 <1984570040@qq.com>
2022-04-12 01:15:49 +0800

    new code style with new eslint

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,31 +1,31 @@
-import { Nav } from '@douyinfe/semi-ui'
-import { useNavigate } from 'react-router-dom'
+import { Nav } from '@douyinfe/semi-ui';
+import { useNavigate } from 'react-router-dom';
 
 type NavigationProps = {
     indexNav: { imgSrc: JSX.Element, title: string, path: string }, 
     itemPath: { title: string, path: string }[]
 };
 export default function Navigation(props: NavigationProps){
-	const navigateTo = useNavigate()
+  const navigateTo = useNavigate();
 
-	return (
-		<Nav mode='horizontal' style={{display: 'flex', flexShrink: '0'}}>
-			<Nav.Item
-				icon={props.indexNav.imgSrc}
-				itemKey={'Index'}
-				text={props.indexNav.title}
-				onClick={() => navigateTo(props.indexNav.path)}
-				style={{color: 'black'}}
-			></Nav.Item>
-			{props.itemPath.map((item, index) => (
-				<Nav.Item 
-					key={item.path}
-					itemKey={index}
-					text={item.title}
-					onClick={() => navigateTo(item.path)}
-				>
-				</Nav.Item>
-			))}
-		</Nav>
-	)
+  return (
+    <Nav mode='horizontal' style={{display: 'flex', flexShrink: '0'}}>
+      <Nav.Item
+        icon={props.indexNav.imgSrc}
+        itemKey={'Index'}
+        text={props.indexNav.title}
+        onClick={() => navigateTo(props.indexNav.path)}
+        style={{color: 'black'}}
+      ></Nav.Item>
+      {props.itemPath.map((item, index) => (
+        <Nav.Item 
+          key={item.path}
+          itemKey={index}
+          text={item.title}
+          onClick={() => navigateTo(item.path)}
+        >
+        </Nav.Item>
+      ))}
+    </Nav>
+  );
 }
--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,20 +1,20 @@
-import Navigation from '../components/nav'
-import styles from './layouts.module.scss'
-import bookLogo from '../source/bookLogo.svg'
+import Navigation from '../components/nav';
+import styles from './layouts.module.scss';
+import bookLogo from '../source/bookLogo.svg';
 
 export default function Layouts(props: {children: JSX.Element}){
 
-	return (
-		<div className={styles.windowContainer}>
-			<Navigation
-				indexNav={{imgSrc: <img src={bookLogo} alt='index' />, title: '阅马平台', path: '/'}}
-				itemPath={[
-					{ title: '活动详情', path: '/activities' }
-				]}
-			></Navigation>
-			<div className={styles.showPageContainer}>
-				{props.children}
-			</div>
-		</div>
-	)
+  return (
+    <div className={styles.windowContainer}>
+      <Navigation
+        indexNav={{imgSrc: <img src={bookLogo} alt='index' />, title: '阅马平台', path: '/'}}
+        itemPath={[
+          { title: '活动详情', path: '/activities' }
+        ]}
+      ></Navigation>
+      <div className={styles.showPageContainer}>
+        {props.children}
+      </div>
+    </div>
+  );
 }
