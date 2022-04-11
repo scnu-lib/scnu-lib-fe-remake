8115cb34d49df34457505fffb38d1818dfab94fd
YXS-2000 <1984570040@qq.com>
2022-04-12 00:32:50 +0800

    add code lint

--- src/pages/activities-detail/index.tsx
+++ src/pages/activities-detail/index.tsx
@@ -2,7 +2,7 @@
 
 export default function ActivitiesDetail(){
 
-    return (
-        <div>this will be used to show activities</div>
-    )
+	return (
+		<div>this will be used to show activities</div>
+	)
 }
