6d8184f0414d14ec3bdc06ba2d162d5028fc092d
okkjoo <keyjo_zhou@m.scnu.edu.cn>
2022-06-15 14:42:39 +0800

    [style] single quotes

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,25 +1,25 @@
-import Navigation from "../components/Nav";
-import styles from "./layouts.module.scss";
-import bookLogo from "../source/bookLogo.svg";
+import Navigation from '../components/Nav';
+import styles from './layouts.module.scss';
+import bookLogo from '../source/bookLogo.svg';
 
 export default function Layouts(props: { children: JSX.Element }) {
-	return (
-		<div className={styles.windowContainer}>
-			<Navigation
-				indexNav={{
-					imgSrc: <img src={bookLogo} alt="index" />,
-					title: "阅马平台",
-					path: "/",
-				}}
-				itemPath={[
-					{ title: "活动列表", path: "/activities" },
-					{
-						title: "我的活动",
-						subRouter: [{ title: "普通活动" }, { title: "志愿者活动" }],
-					},
-				]}
-			/>
-			<div className={styles.showPageContainer}>{props.children}</div>
-		</div>
-	);
+  return (
+    <div className={styles.windowContainer}>
+      <Navigation
+        indexNav={{
+          imgSrc: <img src={bookLogo} alt='index' />,
+          title: '阅马平台',
+          path: '/',
+        }}
+        itemPath={[
+          { title: '活动列表', path: '/activities' },
+          {
+            title: '我的活动',
+            subRouter: [{ title: '普通活动' }, { title: '志愿者活动' }],
+          },
+        ]}
+      />
+      <div className={styles.showPageContainer}>{props.children}</div>
+    </div>
+  );
 }
