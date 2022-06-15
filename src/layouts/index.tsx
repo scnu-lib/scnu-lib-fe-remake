eeb8bdce42a4c27e1545fd179f53d1e7803e279f
YXS-2000 <1984570040@qq.com>
2022-06-15 15:47:40 +0800

    change folder name again

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,4 +1,4 @@
-import Navigation from '../components/navigation';
+import Navigation from '../components/nav';
 import styles from './layouts.module.scss';
 import bookLogo from '../source/bookLogo.svg';
 
