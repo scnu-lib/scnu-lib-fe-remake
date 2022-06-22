93b9454ff5587c394d814a6ca3935aab58ff97ba
YXS-2000 <1984570040@qq.com>
2022-06-23 02:54:28 +0800

    new style component

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,12 +1,13 @@
 import Navigation from '../components/Nav';
 import styles from './layouts.module.scss';
-import bookLogo from '../assets/svg/bookLogo.svg';
+import { ReactComponent as BookLogo } from '../assets/svg/bookLogo.svg';
+import { Icon } from '@douyinfe/semi-ui';
 
 export default function Layouts(props: { children: JSX.Element }) {
   return (
     <div className={styles.windowContainer}>
       <Navigation
-        indexNav={{ imgSrc: <img src={bookLogo} alt='index' />, title: '阅马平台', path: '/' }}
+        indexNav={{ imgSrc: <Icon svg={ <BookLogo style={{ width: 20, height: 20 }} /> } />, title: '阅马平台', path: '/' }}
         itemPath={[
           { title: '活动列表', path: '/activities' },
           {
