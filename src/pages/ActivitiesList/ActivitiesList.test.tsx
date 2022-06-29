076078565d7dbcf11e032e8eb5a6c6e929b1d945
clean99 <xff9924@gmai.com>
2022-06-29 17:17:48 +0800

    snapshot for initial state

--- src/pages/ActivitiesList/ActivitiesList.test.tsx
+++ src/pages/ActivitiesList/ActivitiesList.test.tsx
@@ -1,55 +1,67 @@
 import { render, screen } from '@testing-library/react';
+import renderer from 'react-test-renderer';
 import userEvent from '@testing-library/user-event';
 import { activityTags } from '../../constants/constants';
 import ActivitiesList from './index';
 
 describe('<ActivitiesList />', () => {
-  beforeEach(() => {
-    render(<ActivitiesList />);
+  describe('Logical testing', () => {
+    beforeEach(() => {
+      render(<ActivitiesList />);
+    });
+  
+    it('should show initial selector and filter tags', () => {
+      screen.getByText('标签筛选：');
+      activityTags.map(tag => screen.getByText(tag.name));
+    });
+  
+    it('should show tag in filter when tag selected', () => {
+      const unselectedFilter = screen.queryByTestId('selected-filter');
+      const selector = screen.getByText(activityTags[0].name);
+  
+      userEvent.click(selector);
+  
+      const selectedFilter = screen.getByTestId('selected-filter');
+  
+      expect(unselectedFilter).toBeNull();
+      expect(selectedFilter).toBeDefined();
+      expect(selectedFilter.textContent).toBe(activityTags[0].name);
+    });
+  
+    it('should show selected once when double click', () => {
+      const selector = screen.getByText(activityTags[0].name);
+  
+      userEvent.dblClick(selector);
+  
+      const selectedFilter = screen.queryAllByTestId('selected-filter');
+  
+      expect(selectedFilter).toHaveLength(1);
+    });
+  
+    it('should dispear when click close', () => {
+      const selector = screen.getByText(activityTags[0].name);
+  
+      userEvent.click(selector);
+  
+      const selectedFilter = screen.queryByTestId('selected-filter');
+      const selectedFilterCancelButton = screen.getByRole('img');
+  
+      userEvent.click(selectedFilterCancelButton);
+  
+      const selectedFilterAfterClick = screen.queryByTestId('selected-filter');
+  
+      expect(selectedFilter).toBeDefined();
+      expect(selectedFilterAfterClick).toBeNull();
+    });  
   });
-
-  it('should show initial selector and filter tags', () => {
-    screen.getByText('标签筛选：');
-    const tags = activityTags.map(tag => screen.getByText(tag.name));
+  
+  describe('UI testing', () => {
+    it('should render initial UI correctly', () => {
+      const tree = renderer
+        .create(<ActivitiesList />)
+        .toJSON();
+      
+      expect(tree).toMatchSnapshot();
+    });
   });
-
-  it('should show tag in filter when tag selected', () => {
-    const unselectedFilter = screen.queryByTestId('selected-filter');
-    const selector = screen.getByText(activityTags[0].name);
-
-    userEvent.click(selector);
-
-    const selectedFilter = screen.getByTestId('selected-filter');
-
-    expect(unselectedFilter).toBeNull();
-    expect(selectedFilter).toBeDefined();
-    expect(selectedFilter.textContent).toBe(activityTags[0].name);
-  });
-
-  it('should show selected once when double click', () => {
-    const selector = screen.getByText(activityTags[0].name);
-
-    userEvent.dblClick(selector);
-
-    const selectedFilter = screen.queryAllByTestId('selected-filter');
-
-    expect(selectedFilter).toHaveLength(1);
-  });
-
-  it('should dispear when click close', () => {
-    const selector = screen.getByText(activityTags[0].name);
-
-    userEvent.click(selector);
-
-    const selectedFilter = screen.queryByTestId('selected-filter');
-    const selectedFilterCancelButton = screen.getByRole('img');
-
-    userEvent.click(selectedFilterCancelButton);
-
-    const selectedFilterAfterClick = screen.queryByTestId('selected-filter');
-
-    expect(selectedFilter).toBeDefined();
-    expect(selectedFilterAfterClick).toBeNull();
-  });
-
 });
