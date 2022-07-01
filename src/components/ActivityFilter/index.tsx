7f75fe820c632d002d48fad0995128d59d85fb44
clean99 <xff9924@gmai.com>
2022-07-01 12:55:16 +0800

    fix code style problem

--- src/components/ActivityFilter/index.tsx
+++ src/components/ActivityFilter/index.tsx
@@ -3,10 +3,11 @@ import { Tag, Space, Typography } from '@douyinfe/semi-ui';
 import { activityTags } from '../../constants/constants';
 import styles from './index.module.scss';
 import { TagColor } from '@douyinfe/semi-ui/lib/es/tag';
+import { activityTagsType } from '../../types/types';
 
 interface ActivityFilterProps {
   selectedTags: typeof activityTags;
-  handleChangeSelectedTags: (newSelectedTags: typeof activityTags) => void;
+  handleChangeSelectedTags: (newSelectedTags: activityTagsType) => void;
 }
 
 export default function ActivityFilter({ selectedTags, handleChangeSelectedTags }: ActivityFilterProps) {
@@ -18,15 +19,19 @@ export default function ActivityFilter({ selectedTags, handleChangeSelectedTags 
       <Space className={styles.filterTags}>
         <Space className={styles.unselectedTags}>
           {
-            activityTags.map(tag => <Tag 
-              key={tag.name} 
-              color={tag.color as any}
-              onClick={() => selectedTags
-                .includes(tag)
-            || handleChangeSelectedTags(selectedTags.concat(tag))}
-            >
-              { tag.name }
-            </Tag>)
+            activityTags.map(tag => (
+              <Tag 
+                key={tag.name} 
+                color={tag.color as TagColor}
+                onClick={() => {
+                  if(!selectedTags.includes(tag))
+                    handleChangeSelectedTags(selectedTags.concat(tag));
+                }
+                }
+              >
+                { tag.name }
+              </Tag>)
+            )  
           }
         </Space>
         <Space className={styles.selectedTags}>
