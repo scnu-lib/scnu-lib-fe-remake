c17d9529a9446059b9979644e5fa4decb365510c
okkjoo <keyjo_zhou@m.scnu.edu.cn>
2022-07-04 09:37:55 +0800

    [fix] Added missing volunteer related interface

--- src/store/index.ts
+++ src/store/index.ts
@@ -10,17 +10,19 @@ export const isLogin = atom({
 export const activityDetail = atom<ActivityType>({
   key: 'activityDetail',
   default: {
-    title: 'title',
+    title: '周五影院 | 肖申克的救赎',
     register_date: '2022-04-30T00:00:00',
     start_date: '2022-05-30T00:00:00',
     end_date: '2022-06-30T00:00:00',
     img: 'https://clean99.github.io/img/sicpjs/sicpjs.jpeg',
-    description: 'string',
+    description: '这是一部巴拉巴拉',
     max_num_of_people: 50,
     num_of_people: 20,
     spot: 'string',
     tags: [activityTags[3], activityTags[1]],
-    hosts: [],
+    hosts: ['许同学'],
     is_allow_volunteer: true,
+    max_num_of_volunteer: 30,
+    num_of_volunteer: 20
   }
 });
