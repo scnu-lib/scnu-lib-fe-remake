4dce932295ec8d1d0f4000849846bb27a1b08d4c
YXS-2000 <1984570040@qq.com>
2022-07-04 22:22:40 +0800

    add footer icons

--- src/components/LibFooter/index.tsx
+++ src/components/LibFooter/index.tsx
@@ -1,4 +1,8 @@
 import { Typography, Avatar, AvatarGroup } from '@douyinfe/semi-ui';
+import { ReactComponent as QqIcon } from '../../assets/svg/qq.svg';
+import { ReactComponent as WechatIcon } from '../../assets/svg/wechat.svg';
+import { IconGithubLogo, IconUserCardPhone } from '@douyinfe/semi-icons';
+import classNames from 'classnames';
 import styles from './LibFooter.module.scss';
 const { Title, Text } = Typography;
 
@@ -10,18 +14,22 @@ export default function LibFooter(){
       <div className={styles.contentContainer}>
         <Text className={styles.textFont}>负责人: 谭老师</Text>
         <div className={styles.avatarContainer}>
-          <Text className={styles.textFont}>网站开发: </Text>
-          <AvatarGroup>
+          <Text className={styles.textFont}>网站开发：</Text>
+          <AvatarGroup size='extra-small' maxCount={3}>
             <Avatar color='red' alt='Yang'>Y</Avatar>
             <Avatar color='green' alt='Xu Feng'>XF</Avatar>
             <Avatar color='yellow' alt='Zhuang'>Z</Avatar>
+            <Avatar>+3</Avatar>
           </AvatarGroup>
         </div>
         <Text className={styles.textFont}>平台招新: yuquelink</Text>
       </div>
-      <Title className={styles.titleFont}>联系我们</Title>
-      <div className={styles.contentContainer}>
-
+      <Title className={styles.titleFont} style={{marginTop: 30}}>联系我们</Title>
+      <div className={classNames(styles.contentContainer, styles.iconContainer)}>
+        <QqIcon className={styles.iconSize}/>
+        <WechatIcon className={styles.iconSize}/>
+        <IconGithubLogo size='large' className={styles.iconSize} />
+        <IconUserCardPhone size='large' className={styles.iconSize} />
       </div>
     </div>
   );
