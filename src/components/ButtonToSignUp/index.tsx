26c5d1baa967469a763f49182e0f3e81c4050d81
okkjoo <keyjo_zhou@m.scnu.edu.cn>
2022-07-05 16:14:05 +0800

    [feat] button change

--- src/components/ButtonToSignUp/index.tsx
+++ src/components/ButtonToSignUp/index.tsx
@@ -1,8 +1,7 @@
-import {Button} from '@douyinfe/semi-ui';
+import {Button, Popover } from '@douyinfe/semi-ui';
 import {EsignUpFlag} from '../../constants/constants';
 import styles from './index.module.scss';
 import classNames from 'classnames';
-import { useState } from 'react';
 
 interface ButtonToSignUpProps{
   className?: string,
@@ -15,6 +14,7 @@ export default function ButtonToSignUp({isAllowVolunteer, signUpFlag,
   setSignUpFlag, ...props}:ButtonToSignUpProps){
   
   const click = (flag:EsignUpFlag) => {
+    //TODO: 发送请求 结果给 setSignUpFlag
     setSignUpFlag(flag);
   };
 
@@ -29,7 +29,24 @@ export default function ButtonToSignUp({isAllowVolunteer, signUpFlag,
               onClick={() => click(EsignUpFlag.volunteer)} >报名志愿者</Button> 
             : undefined}
         </>
-        : undefined
+        : 
+        <Popover
+          showArrow
+          arrowPointAtCenter
+          content={
+            <article>
+              你已成功报名{signUpFlag === EsignUpFlag.participant ? '活动' : '志愿者'}！
+            </article>
+          }
+          position='top'     
+          style={{
+            backgroundColor: 'rgba(var(--semi-grey-7), 1)',
+            borderColor: 'rgba(var(--semi-grey-7), 1)',
+            color: 'var(--semi-color-white)',
+          }}     
+        >
+          <Button disabled theme='solid' type='warning' style={{width: '200px'}}>已报名{signUpFlag === EsignUpFlag.participant ? '活动' : '志愿者'}</Button>
+        </Popover>
       }
     </div>
   );
